ac6889a62a7b97fd414a119120e12d8adc949cfa
RaisaNuzhat <RaisaNuzhat@users.noreply.github.com>
2024-11-11 14:50:08 +0000

    deploy: 49d831f933a664c78ee9a8dca13abed88cca172b

--- assets/Page2.a578802c.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index.31e2b0f3.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
---
+++ assets/Page2.dce0befa.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.e0f1fe65.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
